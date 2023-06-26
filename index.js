1de5cddd4d13fd3c0461609392923104c7ec20c8
molavi25 <alimolavi025@gmail.com>
2023-06-26 08:17:00 +0330

    first demo of chat app created

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
