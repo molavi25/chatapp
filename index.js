b29a23fe2134fa09d9f2151a2c1ccf4edadef728
molavi25 <alimolavi025@gmail.com>
2023-12-14 14:01:36 +0330

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const port = 4000;
+const port = 2020;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
